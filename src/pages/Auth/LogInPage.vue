85528e7457e0f5b4609f13ee353262921ea81dbc
blackmarllbor0 <3100194@gmail.com>
2023-10-03 19:58:02 +0600

    register new user

--- src/pages/Auth/LogInPage.vue
+++ src/pages/Auth/LogInPage.vue
@@ -15,11 +15,22 @@
       <q-form
         class="q-gutter-md log-in-form"
       >
-        <email-input v-if="logInMethod==='email'" />
+        <span
+          v-if="isRegistered"
+          class="user-registered row flex-center"
+        >Successfully Registration</span>
 
-        <phone-input v-if="logInMethod === 'phoneNumber'" />
+        <email-input
+          v-if="logInMethod==='email'"
+          v-model="email"
+        />
 
-        <password-input />
+        <phone-input
+          v-if="logInMethod === 'phoneNumber'"
+          v-model="phoneNumber"
+        />
+
+        <password-input v-model="password" />
 
         <div
           class="row flex-center"
@@ -30,6 +41,7 @@
             padding="10px 50px"
             label="Log-in"
             outline
+            @click="logIn"
           />
 
           <q-btn
@@ -50,8 +62,63 @@ import EmailInput from 'src/components/Auth/EmailInput.vue';
 import PhoneInput from 'src/components/Auth/PhoneInput.vue';
 import PasswordInput from 'src/components/Auth/PasswordInput.vue';
 import { ref } from 'vue';
+import { useUserStore } from 'src/stores/users';
+import { storeToRefs } from 'pinia';
+import { stringValidator } from 'src/validators/stringValidator';
+import { emailValidators } from 'src/validators/email';
+import { phoneNumberValidators } from 'src/validators/phoneNumber';
+import { passwordValidators } from 'src/validators/password';
+import { api } from 'src/boot/axios';
+import { isAxiosError } from 'axios';
+
+const userStore = useUserStore();
+const { isRegistered } = storeToRefs(userStore);
 
 const logInMethod = ref<'email' | 'phoneNumber'>('email');
+const email = ref<string>('3100194@gmail.com');
+const phoneNumber = ref<string>('');
+const password = ref<string>('Blackmarllbor_0');
+
+const isValidForms = ref<boolean[]>([]);
+
+// We reset the registration status to false
+// in any case so as not to display the registration message.
+setTimeout(() => {
+  userStore.setUserRegisterStatus(false);
+}, 3000);
+
+const logIn = async () => {
+  isValidForms.value = [];
+
+  if (logInMethod.value === 'email') {
+    isValidForms.value.push(stringValidator(email.value, emailValidators));
+  } else {
+    isValidForms.value.push(stringValidator(phoneNumber.value, phoneNumberValidators));
+  }
+
+  isValidForms.value.push(stringValidator(password.value, passwordValidators));
+
+  if (!isValidForms.value.filter((isValid) => !isValid).length) {
+    const body: {[key: string]: string} = { password: password.value };
+    if (logInMethod.value === 'email') {
+      body.email = email.value;
+    } else {
+      body.phoneNumber = phoneNumber.value;
+    }
+    try {
+      const res = await api.post('/auth/log-in', body);
+      const cookieName = 'Authorization';
+      const cookie = (res.headers['set-cookie'] as string[])
+        .find((c) => c.includes(cookieName))
+        ?.match(new RegExp(`^${cookieName}=(.+?);`))
+        ?.[1];
+    } catch (error) {
+      if (isAxiosError(error)) {
+        console.log(error);
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss">
@@ -64,6 +131,11 @@ const logInMethod = ref<'email' | 'phoneNumber'>('email');
 
   .log-in-form {
     margin: 15px 0;
+  }
+
+  .user-registered {
+    font-size: 20px;
+    color: rgb(111, 178, 11);
   }
 }
 </style>
--- src/pages/Auth/RegisterPage.vue
+++ src/pages/Auth/RegisterPage.vue
@@ -29,6 +29,11 @@
           class="row flex-center"
           style="gap: 1rem;"
         >
+          <span
+            v-if="registerError"
+            class="user-already-exist-msg"
+          >Ups... User with this data already exist</span>
+
           <q-btn
             style="color: red"
             padding="10px 50px"
@@ -47,12 +52,19 @@ import EmailInput from 'src/components/Auth/EmailInput.vue';
 import PhoneInput from 'src/components/Auth/PhoneInput.vue';
 import PasswordInput from 'src/components/Auth/PasswordInput.vue';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import { api } from 'src/boot/axios';
 import { stringValidator } from 'src/validators/stringValidator';
 import { nameValidators } from 'src/validators/name';
 import { emailValidators } from 'src/validators/email';
 import { phoneNumberValidators } from 'src/validators/phoneNumber';
 import { passwordValidators } from 'src/validators/password';
+import { isAxiosError } from 'axios';
+import { useUserStore } from 'src/stores/users';
+
+const userStore = useUserStore();
+
+const router = useRouter();
 
 const name = ref<string>('');
 const email = ref<string>('');
@@ -60,6 +72,7 @@ const phoneNumber = ref<string>('+7 ');
 const password = ref<string>('');
 
 const isValidForms = ref<boolean[]>([]);
+const registerError = ref<boolean>(false);
 
 const registerNewUser = async () => {
   isValidForms.value = [];
@@ -70,17 +83,29 @@ const registerNewUser = async () => {
   isValidForms.value.push(stringValidator(password.value, passwordValidators));
 
   if (!isValidForms.value.filter((isValid) => !isValid).length) {
-    await api.post('/user', {
-      name: name.value,
-      email: email.value,
-      phoneNumber: phoneNumber.value,
-      password: password.value,
-    });
+    try {
+      await api.post('/users', {
+        name: name.value,
+        email: email.value,
+        phoneNumber: phoneNumber.value,
+        password: password.value,
+      });
+
+      userStore.setUserRegisterStatus(true);
+      router.push('/log-in');
+    } catch (error) {
+      if (isAxiosError(error) && error.response?.status === 409) {
+        registerError.value = true;
+      } else {
+        console.error(error);
+      }
+    }
   }
 };
 </script>
 
 <style scoped lang="scss">
+@import "../../css/quasar.variables.scss";
 .register-block {
   padding: 25px;
   border: 1px solid #000;
@@ -93,6 +118,11 @@ const registerNewUser = async () => {
     span {
       font-size: 20px;
     }
+  }
+
+  .user-already-exist-msg {
+    color: $main-red-color;
+    font-size: 15px !important;
   }
 }
 </style>
